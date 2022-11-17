79b70149bec8c3bf38b3e15bb814e9a00eaae98a
miseongk <miseongk@naver.com>
2022-11-18 03:42:28 +0900

    feat: connect AWS S3 for uploading image

---
+++ app/util/image.py
@@ -0,0 +1,18 @@
+import boto3
+import os
+
+
+def s3_connection():
+    try:
+        s3 = boto3.client(
+            service_name="s3",
+            region_name="ap-northeast-2",
+            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
+        )
+    except Exception as e:
+        print(e)
+    else:
+        return s3
+
+
